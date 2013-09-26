cc527ef2b5c96d1f18a419f134f00073e5131eb1
Julian Kalbhenn <jkalbhenn@intera.de>
2013-09-26 09:29:38 +0200

    Redmine 2.3.2 plugin installation compatibility

--- init.rb
+++ init.rb
@@ -1,8 +1,16 @@
 require 'redmine'
-require 'dispatcher'
 require 'redmine_spent_time_column/hooks'
 
-Dispatcher.to_prepare do
+if Rails::VERSION::MAJOR < 3
+  require 'dispatcher'
+  object_to_prepare = Dispatcher
+else
+  object_to_prepare = Rails.configuration
+  # if redmine plugins were railties:
+  # object_to_prepare = config
+end
+
+object_to_prepare.to_prepare do
   Issue.send(:include, RedmineSpentTimeColumn::Patches::IssuePatch) unless Issue.include?(RedmineSpentTimeColumn::Patches::IssuePatch)
   Query.send(:include, RedmineSpentTimeColumn::Patches::QueryPatch) unless Query.include?(RedmineSpentTimeColumn::Patches::QueryPatch)
   QueriesHelper.send(:include, RedmineSpentTimeColumn::Patches::QueriesHelperPatch) unless QueriesHelper.include?(RedmineSpentTimeColumn::Patches::QueriesHelperPatch)
@@ -12,7 +20,7 @@ Redmine::Plugin.register :redmine_spent_time_column do
   name 'Redmine Spent Time Column'
   author 'Jan Schulz-Hofen, Planio GmbH'
   description 'This plugin adds a spent time column to issue lists.'
-  version '2.0.0-devel'
+  version '2.0.1'
 
   settings :default => {
     'show_bottom_summary_line_on_issuelists' => '0',
