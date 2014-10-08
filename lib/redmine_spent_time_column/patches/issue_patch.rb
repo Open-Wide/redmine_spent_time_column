4f55cce5cc31c0912dbaeb08599c4018456da33b
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2014-10-08 14:54:31 +0200

    Use only task with no child to compute

--- lib/redmine_spent_time_column/patches/issue_patch.rb
+++ lib/redmine_spent_time_column/patches/issue_patch.rb
@@ -3,7 +3,11 @@ module RedmineSpentTimeColumn
     module IssuePatch
       
       def calculated_spent_hours
-        @calculated_spent_hours ||= self_and_descendants.sum("estimated_hours * done_ratio / 100").to_f || 0.0
+        if self.children?
+          @calculated_spent_hours ||= descendants.sum("estimated_hours * done_ratio / 100").to_f || 0.0
+        else
+          @calculated_spent_hours ||= (estimated_hours.to_f * done_ratio.to_f / 100).to_f || 0.0
+        end
       end
       
       def divergent_hours
@@ -11,7 +15,11 @@ module RedmineSpentTimeColumn
       end
       
       def calculated_remaining_hours
-        @calculated_remaining_hours ||= self_and_descendants.sum("estimated_hours - (estimated_hours * done_ratio / 100)").to_f || 0.0
+        if self.children?
+          @calculated_remaining_hours ||= descendants.sum("estimated_hours - (estimated_hours * done_ratio / 100)").to_f || 0.0
+        else
+          @calculated_spent_hours ||= (estimated_hours.to_f - (estimated_hours.to_f * done_ratio.to_f / 100)).to_f || 0.0
+        end
       end
       
     end
